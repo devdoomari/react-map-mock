6e761541049cf437193bdfd922b658c2e0bf67c0
Dev@Mesh::iMac <devdoomari@gmail.com>
2016-04-22 03:26:34 +0900

    [wip] working on daum map api js file loader

--- src/loader.ts
+++ src/loader.ts
@@ -1,6 +1,15 @@
-import postscribe = require('postscribe');
+import * as Q from 'q';
+const postscribe = require('postscribe');
 
 
 async function load(APIKey: String) {
-  postscribe();
+  const url = `https://apis.daum.net/maps/maps3.js?apikey=${APIKey}&libraries=services`;
+  postscribe(window.document.head, `<script src="${url}"></script>`, {
+    done: ()=> {
+      deferred.resolve();
+    },
+    error: (e)=> {
+      deferred.reject(e);
+    },
+  });
 }
