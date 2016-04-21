b739f3ba2041140a9343a5b90ea197763e47e9c6
Dev@Mesh::iMac <devdoomari@gmail.com>
2016-04-22 03:41:25 +0900

    [wip] api loader

--- src/api-loader.ts
+++ src/api-loader.ts
@@ -1,9 +1,12 @@
 import * as Q from 'q';
 const postscribe = require('postscribe');
 
+const deferred = Q.defer();
+const promise = deferred.promise;
 
-async function load(APIKey: String) {
+function load(APIKey: String) {
   const url = `https://apis.daum.net/maps/maps3.js?apikey=${APIKey}&libraries=services`;
+  if (promise.)
   postscribe(window.document.head, `<script src="${url}"></script>`, {
     done: ()=> {
       deferred.resolve();
